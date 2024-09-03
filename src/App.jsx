9bd6a75e3f24bdd2406ff27d02b507e6601553b8
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-03 18:09:46 +0530

    code without state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react"
 
 function App() {
 
+  const [count,setCount] = useState(0);
+
   return (
     <>
+    <div>
+        <Parent count={count} setCount={setCount}></Parent>
+    </div>
     </>
+  )
+}
+
+function Parent({count,setCount}) {
+  return(
+    <div>
+      <Child1 count={count}></Child1>
+      <Child2 setCount={setCount}></Child2>
+    </div>
+  )
+}
+
+function Child1(props) {
+  return (
+    <div>{props.count}</div>
+  ) 
+}
+
+function Child2({setCount}) {
+
+  function handleDecrement() {
+    setCount(count => count-1)
+  }
+
+  function handleIncrement() {
+    setCount(count => count+1)
+  }
+
+  return (
+    <div>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
   )
 }
 
